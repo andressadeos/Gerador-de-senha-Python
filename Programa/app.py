ef9a863ed8435a7b5936e2867ac01cc75abda4b9
andressadeos <andressadeoliveirafinanc@gmail.com>
2021-01-22 21:18:05 -0300

    editado README.md e Iniciado Interface grafica do app.py

--- Programa/app.py
+++ Programa/app.py
@@ -1,5 +1,50 @@
 import string
+from string import *
 import random
+import tkinter 
+from tkinter import * #importando o pacote
+
+app=Tk() #instanciando a classe
+app.title("TBank") #definindo o titulo do app
+app.geometry("800x600") #definindo o tamanho da tela do app
+
+texto = Label(app, text="Bem-vindo ao TBank!", font="ArialBlack 20 bold")
+texto.pack()
+
+texto = Label(app, text="Selecione uma opção", font="Arial 16")
+texto.pack()
+
+
+#criar checkbox 01 tela
+#Caixa
+#Gerencia PJ
+#Gerencia PF
+
+#associar uma função a checkbox 01 tela
+
+botao = Button(app, text="Próximo")
+botao.pack()
+
+#criar checkbox 02 tela
+#Preferencial
+#Não Preferencial
+
+#associar uma função a checkbox 02 tela
+
+
+#botao = Button(app, text="Próximo")
+#botao.pack()
+
+#criar 03 tela
+#imput numero celular
+#botao = Button(app, text="Enviar")
+#botao.pack()
+
+#associar uma função a checkbox 03 tela
+
+app.mainloop() #colocando o app em loop para permanecer aberto até que o usuario o feche    
+
+
 
 #determinar senha aleatória
-NUMBER = strings.digits
+#NUMBER = strings.digits
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+## Gerador de senha com Python :snake: ##
 
+O objetivo desse projeto é um programa utilizando linguagem Python com interface gráfica utilizando o pacote TKinter.
+
+
+
+**Material de apoio:**
+
+- https://tiforadacaixa.blogspot.com/p/curso-de-tkinter-interface-trafica-para.html;
+- https://www.devmedia.com.br/tkinter-interfaces-graficas-em-python/33956;
+
+**IDE utilizada:**
+
+- Visual Studio Code
+
+**FUNÇÃO DO PROGRAMA:**
+
+O programa gera senhas aleatórias e envia a senha por celular.
